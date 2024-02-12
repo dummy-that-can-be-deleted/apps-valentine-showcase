dbd55a595e1ec4885a2b77e102c62f988bff4814
Panda Eat Bamboo <2495nmhst@relay.firefox.com>
2024-02-12 10:13:40 +0700

    feat: adding config to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: "https://dummy-that-can-be-deleted.github.io",
+  base: "/apps-valentine-showcase"
 });
